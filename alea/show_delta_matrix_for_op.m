13daeef7c00839011af174020d48143f19065dc1
Elmar Zander <e.zander@tu-bs.de>
2013-09-02 20:06:04 +0200

    added code to compare output with alea

---
+++ alea/show_delta_matrix_for_op.m
@@ -0,0 +1,14 @@
+clear
+clf
+
+I_u=multiindex(3, 2, 'ordering', 'uqtk');
+V_u=gpcbasis_create('p', 'I', I_u);
+
+I_a = multiindex(3, 1, 'ordering', 'uqtk');
+V_a=gpcbasis_create('p', 'I', I_a);
+
+
+L = gpc_triples(V_u, V_u, V_a);
+
+
+spy2(sum(L(:,:,2:end),3))
