04876cf87275e1f48a9960f10022a81aa9dc7ae8
Elmar Zander <e.zander@tu-bs.de>
2013-12-02 22:17:49 +0100

    Squashed 'sglib/' changes from 367c793..07b48f5
    07b48f5 Some separation of function under 'fem'
    18d2737 Added comments to spatial_function stuff
    b4b4b15 Fixed a bug introduced by "performance hints"
    f298ee0 Moved and improved pde toolbox functions
    5b2bf65 Fixed Contents.m for simplefem plus some aesthetic issues
    c4ae1b7 Cleanup of fem/simplefem and related functions
    
    git-subtree-dir: sglib
    git-subtree-split: 07b48f5ca11c0a076ba64e0de5108c86c90f073c

--- fem/simplefem/Contents.m
+++ fem/simplefem/Contents.m
@@ -2,31 +2,21 @@
 % =========
 %
 % This directory contains functions to setup and solve small FEM problems.
-% This is really basic and fragmentary and only meant as a small and easy
-% support for testing the stochastic Galerkin methods, without the hassle
-% of calling external frameworks.
+% This is really basic and only meant as a small and lightweight support
+% for testing the stochastic Galerkin methods, without the need to install
+% and call external frameworks. 
 %
 % PDE and mesh methods
-%   clear_non_boundary_values - 
-%   correct_mesh              - 
-%   create_mesh_2d_rect       - Short description of create_mesh_2d_rect.
-%   create_mesh_1d            - Creates a 1D mesh for simple finite element calculations.
-%   find_boundary             - 
-%   mass_matrix               - Assemble the mass matrix.
-%   mesh_parameters           - 
-%   point_projector           - Short description of point_projector.
-%   refine_mesh               - 
-%   stiffness_matrix          - Assemble stiffness matrix for linear tri/tet elements.
+%   create_mesh_2d_rect          - Creates a mesh of the unitsquare.
+%   create_mesh_1d               - Creates a 1D mesh for simple finite element calculations.
+%   mass_matrix                  - Assemble the mass matrix.
+%   stiffness_matrix             - Assemble stiffness matrix for P1 elements.
 %
 % Unittests
-%   unittest_correct_mesh     - Test the CORRECT_MESH function.
-%   unittest_create_mesh_1d   - Test the CREATE_MESH_1D function.
-%   unittest_find_boundary    - Test the FIND_BOUNDARY function.
-%   unittest_mass_matrix      - Test the mass_matrix function.
-%   unittest_point_projector  - Test the POINT_PROJECTOR function.
-%   unittest_refine_mesh      - Test the REFINE_MESH function.
-%   unittest_stiffness_matrix - UNITTEST_STIFFNESS Test the stiffness_matrix function.
-
+%   unittest_create_mesh_1d      - Test the CREATE_MESH_1D function.
+%   unittest_create_mesh_2d_rect - Test the CREATE_MESH_2D_RECT function.
+%   unittest_mass_matrix         - Test the MASS_MATRIX function.
+%   unittest_stiffness_matrix    - Test the STIFFNESS_MATRIX function.
 
 %   Elmar Zander
 %   Copyright 2009, Institute of Scientific Computing, TU Braunschweig.
