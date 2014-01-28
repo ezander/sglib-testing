4aa86081ddecd203cd21c3d6518c090e4d7765ed
Elmar Zander <e.zander@tu-bs.de>
2014-01-28 08:40:09 +0100

    Split and added stuff to the parameter expansion

--- demo/tutorial/demo_simplefem.m
+++ demo/tutorial/demo_simplefem.m
@@ -5,6 +5,9 @@
 % field described by infinitely many stochastic variable, which has to be
 % (stochastically) discretised beforehand.
 
+
+
+
 %% The deterministic problem
 % Lets look at the following boundary value problem 
 %
@@ -26,6 +29,9 @@ a1 = 2; a2 = 100;
 plot(pos, u); 
 
 
+
+
+
 %% Stochastic description of the parameters
 % Now suppose the parameters a_1 and a_2 are uncertain and described by
 % by random variables. Suppose a_1 follows a Beta[1.2,2] distribution,
@@ -34,15 +40,15 @@ plot(pos, u);
 % |gendist_create| and |gendist_fix_bounds| functions (see
 % demo_distributions). 
 
-dist_a1 = gendist_create('beta', {1.2, 2});
-dist_a1 = gendist_fix_bounds(dist_a1, 0.5, 5);
+a1_dist = gendist_create('beta', {1.2, 2});
+a1_dist = gendist_fix_bounds(a1_dist, 0.5, 5);
 
 %%
 % The probability distribution function (pdf) and the cumulative
 % distribution function (cdf) can be computed with |gendist_cdf| and
 % |gendist_pdf| function 
 x = linspace(0, 7);
-plot(x, gendist_pdf(x, dist_a1), x, gendist_cdf(x, dist_a1));
+plot(x, gendist_pdf(x, a1_dist), x, gendist_cdf(x, a1_dist));
 legend( 'pdf a1', 'cdf a1');
 
 %%
@@ -50,12 +56,16 @@ legend( 'pdf a1', 'cdf a1');
 % distribution with support on [50, 150]. The distribution of a_2 has
 % sharp singularities at both ends.
 
-dist_a2 = gendist_create('beta', {0.6, 0.3});
-dist_a2 = gendist_fix_bounds(dist_a2, 50, 150);
+a2_dist = gendist_create('beta', {0.6, 0.3});
+a2_dist = gendist_fix_bounds(a2_dist, 50, 150);
 
 x = linspace(0, 200, 1000);
-plot(x, gendist_pdf(x, dist_a2), x, gendist_cdf(x, dist_a2));
+plot(x, gendist_pdf(x, a2_dist), x, gendist_cdf(x, a2_dist));
 legend( 'pdf a2', 'cdf a2');
+
+
+
+
 
 %% GPC approximation of the parameters
 % For some methods we could directly use the distributions given above but
@@ -65,66 +75,42 @@ legend( 'pdf a2', 'cdf a2');
 % For $a_1$ the distribution looks quite a bit like a (Wigner) semicircle
 % distribution for which we can use the combination Semicircle/ChebyshevU,
 % specified in sglib by the letter 'u' (small 'u' for the normalised
-% polynomials). 
+% polynomials). With the option 'varerr' we instruct |gpc_param_expand| to
+% choose such a degree of the gpc polynomial, that the error in variance is
+% below 0.001. The option 'fixvar' rescale the gpc coefficients such that
+% the variance is fully matched (possibly incurring a worse match in higher
+% moments).
 % 
-% So the first thing we need to do is create a basis for the GPC space for
-% the first parameter. 
-V1 = gpcbasis_create('u', 'p', 2);
+[a1_alpha, V1, err] = gpc_param_expand(a1_dist, 'u', 'varerr', 0.001, 'fixvar', true);
 
 %%
-% In general we can use now the |gpc_param_coeffs| functions to get the
-% coefficients of the GPC basis function for the given parameter. However,
-% in this demonstration we want to do that stepwise. First, we get the
-% integration points and weights of a Gauss rule with respect to the
-% probablity measure of the GPC basis. Here the point would be therefore
-% the roots of the ChebyshevU polynomial. A rule of order 4 will be here
-% more than sufficient:
-[x,w]=gpc_integrate([], V1, 4);
+% Compare the first four moments of the true distribution of a1 and its GPC
+% approximation
 
-%%
-% The first parameter in the call to |gpc_integrate| would have been a
-% function to integrate. Here, however, we want to get the points and
-% weights and do the integration ourselves.
-%
+[mean,var,skew,kurt]=gendist_moments(a1_dist);
+fprintf('Moments (true):\nmean=%g, var=%g, skew=%g, kurt=%g\n', mean, var, skew, kurt)
+[mean,var,skew,kurt]=gpc_moments(a1_alpha, V1);
+fprintf('Moments (gpc): \nmean=%g, var=%g, skew=%g, kurt=%g\n', mean, var, skew, kurt)
 
-%%
-% $$ F_{a_1}^{-1}(F_S(\cdot))$$ 
 %
-% integrate
-%
-% $$ a1_i = \int_{-1}^1 F_{a_1}^{-1}(F_S(\xi)) U_i(\xi) f_s(\xi) d\xi $$ 
-% 
-% $$ a1_i \approx \sum_k F_{a_1}^{-1}(F_S(x_k)) U_i(x_k) w_k$$ 
-%
-psi_k_alpha = gpcbasis_evaluate(V1,x, 'dual', true);
-fun_k = gendist_invcdf(gpcgerm_cdf(V1, x), dist_a1{:});
-a1_alpha = fun_k*diag(w)*psi_k_alpha;
-
-%% 
 % Plot a kernel density estimate of the gpc approximation of a1 and compare
 % to the true distribution
-% (there should be a method for
-% distribution plotting, rug_plot should determine m automatically) 
+% (there should be a method for distribution plotting) 
 a1_samples = gpc_evaluate(a1_alpha, V1, gpcgerm_sample(V1, 100000)); 
 kernel_density(a1_samples, 100); hold all;
 x=linspace(0,7);
-plot(x, gendist_pdf(x, dist_a1{:})); hold off;
+plot(x, gendist_pdf(x, a1_dist{:})); hold off;
 legend('gpc approx. (kde)', 'exact density');
-rug_plot(a1_samples(1:300));
+rug_plot(a1_samples(1:300), 'color', [0.7,0,0]);
 
-
+% It can be seen in the plot 
 
 %% 
 %For $a_2$ the distribution looks quite a bit like an arcsine
 % distribution for which we can use the combination Arcsine/ChebyshevT,
 % specified in sglib by the letter 't'.
-p2 = 4;
-V2 = gpcbasis_create('t', 'p', p2);
-[x,w]=gpc_integrate([], V2, 10);
 
-psi_k_alpha = gpcbasis_evaluate(V2,x, 'dual', true);
-fun_k = gendist_invcdf(gpcgerm_cdf(V2, x), dist_a2{:});
-a2_alpha = w'*binfun(@times, psi_k_alpha, fun_k');
+[a2_alpha, V2] = gpc_param_expand(a2_dist, 't', 'varerr', 0.01);
 
 %% 
 % Plot a kernel density estimate of the gpc approximation of a2 and compare
@@ -132,9 +118,9 @@ a2_alpha = w'*binfun(@times, psi_k_alpha, fun_k');
 a2_samples = gpc_evaluate(a2_alpha, V2, gpcgerm_sample(V2, 100000));
 kernel_density(a2_samples, 100, 0.01); hold all;
 x=linspace(0,2.5);
-plot(x, gendist_pdf(x, dist_a2{:})); hold off;
+plot(x, gendist_pdf(x, a2_dist{:})); hold off;
 legend('gpc approx. (kde)', 'exact density');
-rug_plot(a2_samples(1:300), 3);
+rug_plot(a2_samples(1:300));
 
 
 
@@ -146,36 +132,36 @@ rug_plot(a2_samples(1:300), 3);
 %
 
 %%
-dist_a1 = gendist_create('beta', {1.2, 2});
-dist_a1 = gendist_fix_bounds(dist_a1, 0.5, 5);
+a1_dist = gendist_create('beta', {1.2, 2});
+a1_dist = gendist_fix_bounds(a1_dist, 0.5, 5);
 
 format compact
 format short g
 for p=1:10
-    [a1_alpha, V1] = gpc_param_expand(dist_a1, 'u', p);
+    [a1_alpha, V1] = gpc_param_expand(a1_dist, 'u', p);
     a1_alpha
-    [mu,var] = gendist_moments(dist_a1)
+    [mu,var] = gendist_moments(a1_dist)
     [mu,var] = gpc_moments(a1_alpha, V1)
 end
 
 
 %%
-dist_a2 = gendist_create('beta', {0.6, 0.3});
-dist_a2 = gendist_fix_bounds(dist_a2, 50, 150);
+a2_dist = gendist_create('beta', {0.6, 0.3});
+a2_dist = gendist_fix_bounds(a2_dist, 50, 150);
 format compact
 format short g
 for p=1:10
-    [a2_alpha, V2] = gpc_param_expand(dist_a2, 't', p);
+    [a2_alpha, V2] = gpc_param_expand(a2_dist, 't', p);
     a2_alpha
-    [mu,var] = gendist_moments(dist_a2)
+    [mu,var] = gendist_moments(a2_dist)
     [mu,var] = gpc_moments(a2_alpha, V2)
 end
 
 
 %% Deeper parameter expansion studies
 % Study the dependence on the polynomial base
-dist_a1 = gendist_create('beta', {1.2, 2});
-dist_a1 = gendist_fix_bounds(dist_a1, 0.5, 5);
-[a1_alpha, V1] = gpc_param_expand(dist_a1, 'p', 'varerr', 0.001)
+a1_dist = gendist_create('beta', {1.2, 2});
+a1_dist = gendist_fix_bounds(a1_dist, 0.5, 5);
+[a1_alpha, V1] = gpc_param_expand(a1_dist, 'p', 'varerr', 0.001)
 [m1,m2,m3,m4]=gpc_moments(a1_alpha, V1); [m1,m2,m3,m4]
-[m1,m2,m3,m4]=gendist_moments(dist_a1); [m1,m2,m3,m4]
+[m1,m2,m3,m4]=gendist_moments(a1_dist); [m1,m2,m3,m4]
