3275d34555163e4c18520842f9091b431e49a0b6
Elmar Zander <e.zander@tu-bs.de>
2014-02-21 22:54:57 +0100

    added plotting function for gpc diffusion demo

---
+++ demo/tutorial/plot_response_surface_results.m
@@ -0,0 +1,21 @@
+subplot(1,2,1); 
+[u_mean, u_var] = gpc_moments(u_i_alpha, V_u);
+plot(pos, u_mean-sqrt(u_var), pos, u_mean, pos, u_mean+sqrt(u_var));
+hold on;
+
+xi_plot = gpcgerm_sample(V_u, 5, 'mode', 'qmc');
+a_plot = gpc_evaluate(a_i_alpha, V_a, xi_plot);
+for i=1:5
+    plot(pos, gpc_evaluate(u_i_alpha, V_u, xi_plot(:,i)), 'k:' );
+    [u_ex, minfo] = model_solve(minfo, a_plot(:,i));
+    plot(pos, u_ex, 'r:');
+end
+legend('mean-std', 'mean', 'mean+std', 'surrogate samples', 'exact samples'); 
+title('resp. surf. proj.'); ylim([0,3.5]); grid on; hold off;
+
+
+
+subplot(1,2,2); 
+plot_response_surface(u_i_alpha([10,30,60,90],:), V_u); 
+title('response surfaces at 0.1, 0.3, 0.6 and 0.9'); zlim([0, 5]);
+
