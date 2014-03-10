f323d7c361d4ea37b2c80e563ebd40a61a28ecaf
Elmar Zander <e.zander@tu-bs.de>
2014-03-10 21:22:07 +0100

    Added gradient line search and CallStatistics

--- tests/bfgs/test_tensor_optim.m
+++ tests/bfgs/test_tensor_optim.m
@@ -20,23 +20,30 @@ eig(A.asmatrix())
 cond(A.asmatrix())
 
 % solve by quasi newton stuff
-func = funcreate(@myfunctional, A, b, @funarg);
-
-
 qnewton_opts.abstol = 1e-5;
 qnewton_opts.verbosity = 1;
 qnewton_opts.maxiter= 1000;
-qnewton_opts.line_search_func = @line_search_armijo;
-qnewton_opts.line_search_opts = {'stretch', true, 'alpha0', 0.5};
-
-
+if false
+    func = funcreate(@myfunctional, A, b, @funarg);
+    qnewton_opts.line_search_func = @line_search_armijo;
+    qnewton_opts.line_search_opts = {'stretch', true, 'alpha0', 1.5};
+else
+    func = funcreate(@myfunctional_grad, A, b, @funarg);
+    qnewton_opts.line_search_func = @line_search_gradient;
+    qnewton_opts.line_search_opts = {'alpha0', 2};
+end
+[func, stats] = CallStatistics.wrap_function(func);
 I=IdentityOperator.from_vector(b);
 H0 = inv(LBFGSOperator(I, 30));
-x0 = 0*b;
+x0 = tensor_null(b);
 x = minfind_quasi_newton(func, x0, H0, qnewton_opts);
-
+stats
 tensor_error(x, x_ex, 'relerr', true)
 
+
+function [f,df]=myfunctional_grad(A, b, x)
+df = -tensor_add(b, A*x, -1);
+f = nan;
 
 function [f,df]=myfunctional(A, b, x)
 y=A*x;
