82aaf409bb3dcf7a395690d3860b5f445770a3e8
Elmar Zander <e.zander@tu-bs.de>
2015-05-21 10:36:27 +0200

    [wip] Some cleanup in norta

--- tests/norta/demo_norta1.m
+++ tests/norta/demo_norta1.m
@@ -5,8 +5,8 @@ X2 = gendist_create('lognormal', {1, 0.4});
 xi1 = gendist_sample(N, X1);
 xi2 = gendist_sample(N, X2);
 multiplot_init(4);
-multiplot; plot_density(X1); hold all; plot_density(xi1, 'type', 'kde');
-multiplot; plot_density(X2); hold all; plot_density(xi2, 'type', 'kde');
+multiplot; plot_density(X1); hold all; plot_density(xi1);
+multiplot; plot_density(X2); hold all; plot_density(xi2);
 %multiplot_adjust_range
 
 rho_N = 0.4;
@@ -17,7 +17,6 @@ rho_N1 = transform_cov_to_normal_fsolve(X1, X2, rho_X, true)
 rho_X = transform_cov_from_normal_qmc(X1, X2, rho_N)
 rho_N1 = transform_cov_to_normal_fsolve(X1, X2, rho_X, false)
 
-%p = setup_polynomial(X1, X2, 10, rho_X)
 rho_N1 = transform_cov_to_normal_hermite(X1, X2, rho_X, 3)
 rho_N1 = transform_cov_to_normal_hermite(X1, X2, rho_X, 4)
 rho_N1 = transform_cov_to_normal_hermite(X1, X2, rho_X, 5)
