c070413bb5d08ca2809cb3f8f2f235b8e56d7f1d
Elmar Zander <e.zander@tu-bs.de>
2015-06-30 15:33:49 +0200

    [wip] Fourier in 2d

--- tests/fourier/kd_fourier.m
+++ tests/fourier/kd_fourier.m
@@ -1,4 +1,4 @@
-function [sigma_k, wp_k]=kd_fourier(func, pos, varargin)
+function [sigma_k, wp_k, r_k]=kd_fourier(func, pos, varargin)
 % KD_FOURIER Short description of kd_fourier.
 %   KD_FOURIER(VARARGIN) Long description of kd_fourier.
 %
@@ -18,7 +18,8 @@ function [sigma_k, wp_k]=kd_fourier(func, pos, varargin)
 %   program.  If not, see <http://www.gnu.org/licenses/>.
 
 options=varargin2options(varargin, mfilename);
-[K,options]=get_option(options, 'max_funcs', 50);
+[K,options]=get_option(options, 'max_funcs', 2000);
+[ratio,options]=get_option(options, 'ratio', 0.99);
 [is_spectral,options]=get_option(options, 'is_spectral', false);
 [autoenlarge,options]=get_option(options, 'autoenlarge', true);
 check_unsupported_options(options);
@@ -34,9 +35,9 @@ end
 
 while true
     if is_spectral
-        [S_k, wp_k] = power_spectrum_by_density(func, L, K);
+        [S_k, wp_k] = power_spectrum_by_density(func, L, K, d);
     else
-        [S_k, wp_k] = power_spectrum_by_fft(func, L, K);
+        [S_k, wp_k] = power_spectrum_by_fft(func, L, K, d);
     end
     if autoenlarge && sum(S_k)-1>1e-7
         L(1) = L(1) * 1.5;
@@ -48,21 +49,74 @@ end
 
 s_k=real(sqrt(S_k));
 s_k=reshape(s_k,[],1);
-sigma_k=[s_k(1); s_k(2:end); s_k(2:end)];
-wp_k = [wp_k; wp_k(2:end,1), zeros(K,1)];
+[s_k, wp_k] = sort_spectrum(s_k, wp_k);
+[s_k, wp_k] = limit_spectrum(s_k, wp_k, ratio);
+[sigma_k, wp_k] = add_sines(s_k, wp_k, d);
 
 if ~isempty(pos)
-    r_k = trig_basis_eval(wp_k, pos);
-    u_k = binfun(@times, sigma_k, r_k);
+    r_k = trig_basis_eval(sigma_k, wp_k, pos);
 end
 
-function [S_k, wp_k] = power_spectrum_by_fft(func, L, K)
+function [s_k, wp_k] = sort_spectrum(s_k, wp_k)
+[s_k(2:end), ind] = sort(s_k(2:end));
+wp_k(2:end,:) = wp_k(ind+1,:);
+
+function [s_k, wp_k] = limit_spectrum(s_k, wp_k, ratio)
+r = cumsum(s_k.^2);
+max_ind = find(r>=ratio, 1, 'first');
+if isempty(max_ind)
+    warning('sglib:kd_fourier', 'Not enough basis functions for given ratio. Increase max_funcs option');
+else
+    s_k = s_k(1:max_ind);
+    wp_k = wp_k(1:max_ind,:);
+end
+
+function [s_k, wp_k] = add_sines(s_k, wp_k, d)
+for i=1:d
+    s_k=[s_k(1); s_k(2:end); s_k(2:end)];
+    wp_k_s = wp_k(2:end,:);
+    wp_k_s(:,2*i)=0;
+    wp_k = [wp_k; wp_k_s];
+end
+
+
+function [S_k, wp_k] = power_spectrum_by_fft(func, L, K, d)
 M=2*K+1;
-[S_k, wp_k] = fourier_series_expand(func, -L(1), L(1), M, 'symmetry', 'even');
+if length(L)==1 && d>1
+    L = L * ones(d,1);
+end
+assert(d==1, 'd>1 does not work yet');
+[S_k, wp_k] = fourier_series_expand(func, -L, L, M, 'symmetry', 'even');
 
 
-function [S_k, wp_k] = power_spectrum_by_density(func, L, K)
-w = (0:K)'/(2*L);
-S_k = funcall(func, w')' / (2*L);
-S_k(2:end) = 2*S_k(2:end);
-wp_k = [2*pi*w, repmat(pi/2, size(w))];
+function [S_k, wp_k] = power_spectrum_by_density(func, L, K, d)
+K_i = ceil(K ^ (1/d));
+w_i = {};
+for i = 1:d
+    w_i{i} = (0:K_i)/(2*L(i));
+end
+w = tensor_mesh(w_i, w_i);
+
+S_k = funcall(func, w, d)' / prod(2*L);
+S_k(2:end) = 2^d * S_k(2:end);
+wp_k = [2*pi*w', repmat(pi/2, size(w'))];
+wp_k = [wp_k(:, 1:2:end), wp_k(:,2:2:end)];
+
+function tensorize(s_k, wp_k, d)
+K_i = ceil(K0 ^ (1/d));
+S_k = ones(1);
+wp_k = zeros(1,0);
+K = 1;
+for i = 1:d
+    w = (0:K_i)'/(2*L(i));
+    S_k_i = funcall(func, w', d)' / (2*L(i));
+    S_k_i(2:end) = 2*S_k_i(2:end);
+    wp_k_i = [2*pi*w, repmat(pi/2, size(w))];
+
+    I1 = repmat((1:K)', K_i, 1);
+    I2 = repmat((1:K_i), 1, K);
+    S_k = S_k(I1) .* S_k_i(I2);
+    wp_k = [wp_k(I1,:), wp_k_i(I2,:)];
+    K = K_i * K;
+end
+
--- tests/fourier/test_kd_fourier_2d.m
+++ tests/fourier/test_kd_fourier_2d.m
@@ -1,24 +1,35 @@
+%%
 clf
 
 a=2; b=3.5;
 l_c = 0.2;
 
-cov_func = @(x)(gaussian_covariance(x,[],l_c));
-pow_func = @(w)(gaussian_spectral_density(w,l_c,[]));
-
-
-nu = 0.1;
+nu = 1;
 cov_func = @(x)(matern_covariance(nu, x,[],l_c));
-pow_func = @(w)(matern_spectral_density(nu, w,l_c,[]));
+pow_func = @(w, d)(matern_spectral_density(nu, w, l_c, d));
 
-cov_func = @(x)(exponential_covariance(x,[],l_c));
-pow_func = @(w)(exponential_spectral_density(w,l_c,[]));
+[pos,els]=load_pdetool_geom( 'lshape', 'numrefine', 1 );
+
 
 enlarge=true;
 options = {'autoenlarge', enlarge, 'max_funcs', 300, 'ratio', 0.99};
-[sigma_k, wp_k] = kd_fourier(cov_func, [b-a], options);
-[sigma_k, wp_k] = kd_fourier(pow_func, [b-a], 'is_spectral', true, options{:});
+%[sigma_k, wp_k] = kd_fourier(cov_func, pos, options);
+[sigma_k, wp_k, u_k] = kd_fourier(pow_func, pos, 'is_spectral', true, options{:});
 
+clf
+for i=1:min(size(u_k,1),10)
+    [i, wp_k(i,[1,3])]
+    plot_field(pos, els, u_k(i,:)')
+    drawnow
+    pause(0.1);
+end
+plot_field(pos, els, sum(u_k,1)')
+
+
+%% 
+
+%
+if false
 xi = linspace(a,b,100);
 [x1,x2]=meshgrid(xi);
 C_ex=reshape(cov_func(x1(:)'-x2(:)'), size(x1));
@@ -52,3 +63,4 @@ plot(xi, r_i);
 grid on;
 
 multiplot_adjust_range('axes', 'z', 'cols', [1])
+end
