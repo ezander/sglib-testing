b3938daf776ddf434639c760d639af9c553c0af6
Elmar Zander <e.zander@tu-bs.de>
2015-11-25 11:14:49 +0100

    Moved all the testing stuff into my private repo

--- tests/bfgs/iterplot.m
+++
@@ -1,5 +0,0 @@
-function iterplot(x, xn, plotopts)
-hold on;
-plot([x(1), xn(1)], [x(2), xn(2)], plotopts);
-hold off;
-drawnow;
--- tests/param_estim/zero_find.m
+++
@@ -1,4 +0,0 @@
-function [ind, dir]=zero_find(y)
-ind1 = find(sign(y(1:end-1)) ~= sign(y(2:end)));
-ind = ind1 + y(ind1) ./ (y(ind1) - y(ind1+1));
-dir = sign(y(ind1+1)-y(ind1));
